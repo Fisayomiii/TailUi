d613428c0eae0dd7930be4f45f79ce240f782878
oluwafisayomi <wrld26993@gmail.com>
2022-12-27 08:55:43 +0100

    [ feat ] redesigned the ui for the 404 page

--- src/pages/404/Notfoundpage.tsx
+++ src/pages/404/Notfoundpage.tsx
@@ -3,19 +3,23 @@ import { NavLink } from "react-router-dom";
 function Notfoundpage() {
   return (
     <>
-      <div className="mt-12">
-        <div className="flex justify-center gap-5 h-9 items-center">
+      <br /><br /><br /><br /><br /><br />
+      <div className="flex justify-center items-center mt-12 pt-12">
+        <div className="flex justify-center items-center gap-x-4">
           <h3 className="text-5xl font-semibold text-gray-700">404</h3>
-          <div className="block border-l border-slate-200 pl-6">
-            <span className="text-3xl font-semibold text-sky-700">Page Not Found</span>
-            <NavLink to={"/"}>
-              <div className="flex font-medium">
-                <span >Go Home</span>
-              </div>
-            </NavLink>
+          <div className="block border-l border-slate-400 pl-2">
+            <span className="text-md font-semibold text-sky-700">This Page isn't routed!</span>
+            <p>
+              <NavLink to={'/'}>
+                go back home
+              </NavLink>
+            </p>
           </div>
         </div>
       </div>
+      <footer className="flex justify-center items-center mt-12">
+        <p>Looks like you're lost.</p>
+      </footer>
     </>
   );
 }
